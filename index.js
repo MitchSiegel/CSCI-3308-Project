222dd41d3c42bd2631e3214b340edf089f79d483
Mitch Siegel <mitchsiegel14@gmail.com>
2023-11-10 09:29:15 -0700

    fixing importing

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //super simple to verify docker compose is working and running
-const express = require('express');
 
+
+const express = require('express'); // To build an application server or API
 const app = express();
-const pgp = require('pg-promise')();
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require('bcrypt'); //  To hash passwords
 
 // Authentication Middleware.
 const auth = (req, res, next) => {
@@ -13,7 +17,9 @@ const auth = (req, res, next) => {
     next();
 };
 
-// Authentication Required
+// middleware setup
+app.set('view engine', 'ejs'); // set the view engine to EJS
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
 app.use(auth);
 
 // database configuration
@@ -24,7 +30,6 @@ const dbConfig = {
     user: process.env.POSTGRES_USER, // the user account to connect with
     password: process.env.POSTGRES_PASSWORD, // the password of the user account
 };
-
 
 //init pgp with our db config
 const db = pgp(dbConfig);
@@ -99,9 +104,6 @@ app.post('/login', async (req, res) => {
     }
 
 });
-
-
-
 
 
 //listen for requests
